5e2e8c0c940d2377edb8a1018bb1f7e272d06ba3
robrascl <robras.cl@gmail.com>
2023-04-24 15:23:42 -0400

    Se agrego tooltip y modal

--- assets/js/codigo.js
+++ assets/js/codigo.js
@@ -1,18 +1,31 @@
-$("a").click( function() {
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-    if (this.hash !== "" ){
-        event.preventDefault();
-        var hash = this.hash;
 
-    $('html,body').animate({
-        scrollTop: $(hash).offset().top
-    }, 3800, function() {
-        windows.location.hash = hash;
-    });
+// $(document).ready(function(){
 
-        
+// $("a").on('click', function(event) {
+
+//     if (this.hash !== "" ){
+//         event.preventDefault();
+//         var hash = this.hash;
+
+//         $('html, body').animate({
+//             scrollTop: $(hash).offset().top
+//              }, 2000, function() {
+
+//             windows.location.hash = hash; 
+//              }); 
+
+//     } // cierre if
+
+// }); // cierre  "a" 
+
+// }); // cierre document
+
+$("#correo").click( function(){
+    // $(this).hide();
+     //alert("Gracias por enviarnos su opinion. Nuestro equipo le responderá lo más pronto posible.");
+     document.querySelector('#oculto').click();
     }
-
-
-
-    });
+ );
