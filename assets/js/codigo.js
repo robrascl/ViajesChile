ed336e4ade098ba4e84ef9a03767eb7c9eb5fe80
robrascl <robras.cl@gmail.com>
2023-04-24 19:29:35 -0400

    efecto smooth scrooll arreglado

--- assets/js/codigo.js
+++ assets/js/codigo.js
@@ -2,30 +2,34 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 
-// $(document).ready(function(){
 
-// $("a").on('click', function(event) {
-
-//     if (this.hash !== "" ){
-//         event.preventDefault();
-//         var hash = this.hash;
-
-//         $('html, body').animate({
-//             scrollTop: $(hash).offset().top
-//              }, 2000, function() {
-
-//             windows.location.hash = hash; 
-//              }); 
-
-//     } // cierre if
-
-// }); // cierre  "a" 
-
-// }); // cierre document
 
 $("#correo").click( function(){
-    // $(this).hide();
-     //alert("Gracias por enviarnos su opinion. Nuestro equipo le responderá lo más pronto posible.");
+    
      document.querySelector('#oculto').click();
     }
  );
+
+
+ $("#XXXX").click(function(){
+    alert("Hola");
+    document.querySelector('#vimagen').click();
+    
+
+ });
+
+
+ $(document).ready(function() {
+   $('a.smooth-scroll').click(function(event) {
+       if (this.hash !== "") {
+           event.preventDefault();
+           var hash = this.hash;
+
+           $('html, body').animate({
+               scrollTop: $(hash).offset().top
+           }, 800, function(){
+               window.location.hash = hash;
+           });
+       }
+   });
+});
